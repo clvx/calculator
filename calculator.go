066ce0547ffb070bd356b82d829b990006aec2d9
Luis Michael Ibarra <michael.ibarra@gmail.com>
2024-10-15 11:47:08 -0600

    Update calculator.go
    Divide function

--- calculator.go
+++ calculator.go
@@ -14,3 +14,11 @@ func subtract(x int, y int) int {
 func multiply(x int, y int) int {
 	return x * y
 }
+
+// create a function to divide two numbers
+func divide(x int, y int) (int, error) {
+    if y == 0 {
+        return 0, fmt.Errorf("cannot divide by zero")
+    }
+    return x / y, nil
+}
